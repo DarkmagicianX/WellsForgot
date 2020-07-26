214d119f59ca2497bfd4a456518754fe9af13a5c
hamzah <haldolaay@gmail.com>
2020-07-26 06:22:50 -0700

    gokuSan

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = __dirname;
-app.get("/", (req, res) => {
-  res.sendFile(`${path}/landingPage/landingPage.html`);
-  //   res.send("hello");
-});
+//uses
+app.use("/landingPage", express.static("landingPage"));
+app.use("/login", express.static("loginPage"));
+app.use("/signup", express.static("signupPage"));
+
+//routes
 app
   .route("/landingPage")
   .get((req, res) => {
@@ -33,6 +35,7 @@ app
     res.send("signup request working");
   });
 
+//listen to me Vegeta
 app.listen(port, () => {
   console.log("working");
 });
