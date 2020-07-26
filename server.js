4be2d50223b320ddd6edf846a30bac06686ebe89
hamzah <haldolaay@gmail.com>
2020-07-26 06:13:16 -0700

    added some routes

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ app.get("/", (req, res) => {
   res.sendFile(`${path}/landingPage/landingPage.html`);
   //   res.send("hello");
 });
+app
+  .route("/landingPage")
+  .get((req, res) => {
+    res.send("get request working");
+  })
+  .post((req, res) => {
+    res.send("post request working");
+  });
+
+app
+  .route("/login")
+  .get((req, res) => {
+    res.send("login request working");
+  })
+  .post((req, res) => {
+    res.send("login request working");
+  });
+
+app
+  .route("/signup")
+  .get((req, res) => {
+    res.send("signup request working");
+  })
+  .post((req, res) => {
+    res.send("signup request working");
+  });
 
 app.listen(port, () => {
   console.log("working");
